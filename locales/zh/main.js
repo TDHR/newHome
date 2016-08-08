22e29ecf5473d5d1bf20edff5e7461d75e6de650
Biallo <bialloworld@gmail.com>
2016-08-08 20:32:32 +0800

    Update locales

--- locales/zh/main.js
+++ locales/zh/main.js
@@ -8,5 +8,6 @@
   "nav-team": "团队",
   "nav-knowledge": "知识库",
   "nav-story": "案例",
-  "nav-contact": "联系我们"
+  "nav-contact": "联系我们",
+  "footer-copyright": "&#169; 2016 瑞资（北京）科技有限公司&nbsp;&nbsp; &#124; &nbsp;&nbsp;京ICP备16039726号"
 }
