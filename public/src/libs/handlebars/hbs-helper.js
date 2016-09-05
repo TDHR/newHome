bd1b805ca8ee4e375ff1c4f9f79df8ccb7bd859d
Biallo <bialloworld@gmail.com>
2016-09-05 20:37:19 +0800

    Update

--- public/src/libs/handlebars/hbs-helper.js
+++ public/src/libs/handlebars/hbs-helper.js
@@ -37,11 +37,34 @@ Handlebars.registerHelper('ifCond', function(v1, operator, v2, options) {
   return options.inverse(this);
 });
 
-Handlebars.registerHelper("covertSize", function(value, options) {
-  var sizes = ['KB', 'MB', 'GB', 'TB', 'PB'];
-  if (value == 0) {
-    return '0 KB';
+Handlebars.registerHelper("covertSize", function(bytes, decimals, options) {
+  if (!bytes) {
+    return '0 B';
   }
-  var i = Math.floor(Math.log(value) / Math.log(1000));
-  return (value / Math.pow(1000, Math.floor(i))).toFixed(2) + ' ' + sizes[i];
+  let k = 1000; // 二进制时用 1024
+  decimals = decimals + 1 || 2; // 默认保留两位
+  let sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  let i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(decimals)) + ' ' + sizes[i];
+});
+
+Handlebars.registerHelper("times", function(n, start, block) {
+  var accum = '';
+  start = start || 0;
+  if (start) {
+    for (var i = start; i <= n; ++i) {
+      accum += block.fn(i);
+    }
+  } else {
+    for (var i = start; i < n; ++i) {
+      accum += block.fn(i);
+    }
+  }
+  return accum;
+});
+
+Handlebars.registerHelper("dateFormat", function(timestamp, type) {
+  var fix = type === 'unix' ? 1000 : 1;
+  var date = new Date(timestamp * fix);
+  return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
 });
