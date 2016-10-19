df20df9d5cfbec2104cd878011f2e45dc18e6876
Biallo <bialloworld@gmail.com>
2016-10-19 10:25:55 +0800

    调整「区块链浏览器」页面为登录后可见

--- app/routes/explorer.js
+++ app/routes/explorer.js
@@ -2,5 +2,5 @@ var explorer = require('./../controllers/explorer');
 
 module.exports = function(app, auth) {
   // 首轮天使额度公示页面
-  app.get('/explorer', explorer.angels);
+  app.get('/explorer', auth.needToLogin, explorer.angels);
 };
