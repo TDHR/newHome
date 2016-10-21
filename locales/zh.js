239d691114bc2f81aed7de890cf1fe76f0747c0c
Biallo <bialloworld@gmail.com>
2016-10-21 10:39:31 +0800

    调整文字内容

--- locales/zh.js
+++ locales/zh.js
@@ -25,7 +25,7 @@
 	"angels-presale": "会员预售赠予",
 	"home-intro-title1": "我们是谁",
 	"home-intro-content1": "瑞资（北京）科技有限公司成立于2016年5月，",
-	"home-intro-content2": "是由来自华尔街投行精英、前瑞银环球不动产中国区首席运营官（COO）与两位区块链技术英才共同发起创立的公司。",
+	"home-intro-content2": "是由来自华尔街投行精英、前瑞银集团全球不动产业务大中华区首席运营官（COO）与两位区块链技术英才共同发起创立的公司。",
 	"home-values-title": "核心价值",
 	"home-values-content": "瑞资科技致力于ABC(Asset-Backed-Chain)的搭建,运用区块链技术为资产证券化和房地产投融资服务提供一站式的解决方案；利用区块链公开透明和数字资产高流动性和跨境流动的特点,创建一个面向全球的数字资产交易平台。",
 	"home-more": "了解更多",
