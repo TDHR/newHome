058d20008ad0bd9dcca9bdbec97b847cc4e8aa7e
Biallo <bialloworld@gmail.com>
2016-12-08 15:43:32 +0800

    Update intro

--- public/src/scripts/help-intro.js
+++ public/src/scripts/help-intro.js
@@ -1,3 +1,14 @@
+// plugin
+import Cookies from 'cookies-js';
+
+// modules
+import ChangeLanguage from './modules/change-language';
+
+// 语言切换按钮
+$('#languageBtn').on('click', function() {
+  ChangeLanguage($(this).data('language') === 'zh' ? 'en' : 'zh');
+});
+
 Reveal.initialize({
   center: true,
   history: true,
@@ -18,37 +29,41 @@ Reveal.addEventListener('slidechanged', function(event) {
  * @param {[string]} [page] [当前的页数]
  */
 function ctrAnimate(page) {
-  $('.show-icon').removeClass('show-icon');
+  $('.show-it').removeClass('show-it');
   $('.intro-coin').removeClass('coin-drop');
   $('.rotatey').removeClass('rotatey');
   switch (page) {
     case 2:
-      $('#iconSignUp').addClass('show-icon');
+      $('#picAsset1').addClass('show-it');
+      $('#picAsset2').addClass('show-it');
       break;
     case 3:
-      $('#iconSurvey').addClass('show-icon');
+      $('#iconSurvey').addClass('show-it');
       break;
     case 4:
-      $('#iconCheck').addClass('show-icon');
+      $('#iconCheck').addClass('show-it');
       break;
     case 5:
-      $('#iconFinance').addClass('show-icon');
+      $('#iconFinance').addClass('show-it');
       break;
     case 7:
-      $('#iconChose').addClass('show-icon');
+      $('#iconChose').addClass('show-it');
       break;
     case 8:
       progress();
       break;
     case 9:
-      $('#iconWallet').addClass('show-icon');
+      $('#iconWallet').addClass('show-it');
       setTimeout(function() {
         $('.intro-coin').addClass('coin-drop');
-      }, 1000);
+      }, 500);
       break;
     case 11:
+      $('#picTrade').addClass('show-it');
       break;
     case 12:
+      $('#iconCatcher').addClass('show-it');
+      $('.icon-catcher-coin').addClass('show-it');
       break;
     case 13:
       $('#picRts').addClass('rotatey');
