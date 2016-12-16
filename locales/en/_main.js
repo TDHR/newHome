9220db6ace0db34ecaddfdd1907ad90ec3af709a
Biallo <bialloworld@gmail.com>
2016-12-16 17:51:43 +0800

    bugfixes

--- locales/en/_main.js
+++ locales/en/_main.js
@@ -17,7 +17,7 @@
   "nav-block-explorer": "Blockchain explorer",
   "footer-copyright": "&#169; 2016 REITs (Beijing) FinTech Co., Ltd. All Rights Reserved&nbsp;&nbsp; &#124; &nbsp;&nbsp;京ICP备16039726号-1",
   "browsehappy": "Your browser is ancient! <a href=\"http://browsehappy.com/\">Upgrade to a different browser</a> to experience this site.",
-  "my-assets": "My Assets",
+  "my-account": "My Account",
   "quit": "Quit",
   "submit": "Submit",
   "modify": "Modify",
--- locales/zh/_main.js
+++ locales/zh/_main.js
@@ -17,7 +17,7 @@
   "nav-block-explorer": "区块链浏览器",
   "footer-copyright": "&#169; 2016 REITs (Beijing) FinTech Co., Ltd. All Rights Reserved&nbsp;&nbsp; &#124; &nbsp;&nbsp;京ICP备16039726号-1",
   "browsehappy": "你的浏览器太古老啦! 请<a href=\"http://browsehappy.com/\">升级</a>到其它现代浏览器，以便正常访问本网站。",
-  "my-assets": "我的资产",
+  "my-account": "我的账户",
   "quit": "退出",
   "submit": "提交",
   "modify": "修改",
