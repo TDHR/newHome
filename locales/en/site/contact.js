015a311110717ba7548a1da7143eab1dcd1335c4
Biallo <bialloworld@gmail.com>
2016-12-20 16:33:13 +0800

    修改公司地址

--- locales/en/site/contact.js
+++ locales/en/site/contact.js
@@ -1,6 +1,6 @@
 {
   "contact-card1-name": "REITs (Beijing) FinTech Co., Ltd.",
-  "contact-card1-text1": "Address: Woo Space, JiaRun Garden Building B. WangJing Chaoyang District, Beijing China",
+  "contact-card1-text1": "Address: Room B207, Woo Space, Block F, Wangjing International Business Center, Chaoyang District, Beijing China",
   "contact-card1-text2": "Tel：400-0599980",
   "contact-card1-text3": "Email：bd@reitsfax.com",
   "contact-wechat": "WeChat",
