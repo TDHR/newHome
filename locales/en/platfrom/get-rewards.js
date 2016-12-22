8208b5f5e973ef7994bf044fc38ff41a5b0e357d
Biallo <bialloworld@gmail.com>
2016-12-22 13:49:38 +0800

    Update locale

---
+++ locales/en/platfrom/get-rewards.js
@@ -0,0 +1,13 @@
+{
+  "get-rewards-title": "Invite your friends to register, to get reward",
+  "get-rewards-rules": "Rules: Friends use the invitation code to complete the registration, and through the identity verification, then the system will automatically issue rewards to your account.",
+  "get-rewards-method1": "Method 1: friends in the registration fill in your invitation code",
+  "get-rewards-method2": "Method 2: Copy the following link to send to friends",
+  "get-rewards-my": "My invitation",
+  "get-rewards-phone": "Phone number",
+  "get-rewards-status": "Status",
+  "get-rewards-time": "Registration time",
+  "get-rewards-state0": "Have not done identity verification",
+  "get-rewards-state1": "Under review",
+  "get-rewards-state2": "Success"
+}
---
+++ locales/zh/platfrom/get-rewards.js
@@ -0,0 +1,13 @@
+{
+  "get-rewards-title": "邀请好友注册，即可获得奖励",
+  "get-rewards-rules": "规则：好友使用邀请码完成注册，并且通过了实名认证，那么系统将会自动发放奖励到你的账户。",
+  "get-rewards-method1": "方法一：好友在注册时填写你的邀请码",
+  "get-rewards-method2": "方法二：复制下方链接发送给好友",
+  "get-rewards-my": "我的邀请",
+  "get-rewards-phone": "手机号码",
+  "get-rewards-status": "状态",
+  "get-rewards-time": "注册时间",
+  "get-rewards-state0": "未实名认证",
+  "get-rewards-state1": "实名认证中",
+  "get-rewards-state2": "邀请成功"
+}
