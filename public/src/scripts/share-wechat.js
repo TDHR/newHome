56c1b2ed4e54e9255e7a663430d7b3a68bd57ceb
Biallo <bialloworld@gmail.com>
2017-03-08 17:53:49 +0800

    调整微信分享页面的分享地址

--- public/src/scripts/share-wechat.js
+++ public/src/scripts/share-wechat.js
@@ -65,42 +65,44 @@ function submitForm(data) {
 
 // 设置分享内容
 function setShare(inviteCode) {
+  var url = $('#env').val();
+
   // 分享到朋友圈
   wx.onMenuShareTimeline({
     title: 'Hi 朋友，壕气送你100RTSt，快来领取吧！', // 分享标题
-    link: 'http://test.reitschain.com/signup?i=' + inviteCode, // 分享链接
-    imgUrl: 'http://test.reitschain.com/images/share-wechat.png' // 分享图标
+    link: `${url}/signup?i=${inviteCode}`, // 分享链接
+    imgUrl: `${url}/images/share-wechat.png` // 分享图标
   });
 
   // 分享给朋友
   wx.onMenuShareAppMessage({
     title: 'Hi 朋友，壕气送你100RTSt，快来领取吧！', // 分享标题
     desc: '面向万亿金融市场的区块链应用-瑞资链-正在公测，1000万好礼全民开抢。', // 分享描述
-    link: 'http://test.reitschain.com/signup?i=' + inviteCode, // 分享链接
-    imgUrl: 'http://test.reitschain.com/images/share-wechat.png' // 分享图标
+    link: `${url}/signup?i=${inviteCode}`, // 分享链接
+    imgUrl: `${url}/images/share-wechat.png` // 分享图标
   });
 
   // 分享到QQ
   wx.onMenuShareQQ({
     title: 'Hi 朋友，壕气送你100RTSt，快来领取吧！', // 分享标题
     desc: '面向万亿金融市场的区块链应用-瑞资链-正在公测，1000万好礼全民开抢。', // 分享描述
-    link: 'http://test.reitschain.com/signup?i=' + inviteCode, // 分享链接
-    imgUrl: 'http://test.reitschain.com/images/share-wechat.png' // 分享图标
+    link: `${url}/signup?i=${inviteCode}`, // 分享链接
+    imgUrl: `${url}/images/share-wechat.png` // 分享图标
   });
 
   // 分享到腾讯微博
   wx.onMenuShareWeibo({
     title: 'Hi 朋友，壕气送你100RTSt，快来领取吧！', // 分享标题
     desc: '面向万亿金融市场的区块链应用-瑞资链-正在公测，1000万好礼全民开抢。', // 分享描述
-    link: 'http://test.reitschain.com/signup?i=' + inviteCode, // 分享链接
-    imgUrl: 'http://test.reitschain.com/images/share-wechat.png' // 分享图标
+    link: `${url}/signup?i=${inviteCode}`, // 分享链接
+    imgUrl: `${url}/images/share-wechat.png` // 分享图标
   });
 
   // 分享到QQ空间
   wx.onMenuShareQZone({
     title: 'Hi 朋友，壕气送你100RTSt，快来领取吧！', // 分享标题
     desc: '面向万亿金融市场的区块链应用-瑞资链-正在公测，1000万好礼全民开抢。', // 分享描述
-    link: 'http://test.reitschain.com/signup?i=' + inviteCode, // 分享链接
-    imgUrl: 'http://test.reitschain.com/images/share-wechat.png' // 分享图标
+    link: `${url}/signup?i=${inviteCode}`, // 分享链接
+    imgUrl: `${url}/images/share-wechat.png` // 分享图标
   });
 }
