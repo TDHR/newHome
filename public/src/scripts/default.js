f44c92e6e7f10ff9a9dd489e9cd429821b8cd85b
Biallo <biallo@BiallodeiMac.local>
2017-05-06 20:28:58 +0800

    调整官网

--- public/src/scripts/default.js
+++ public/src/scripts/default.js
@@ -1,12 +1,2 @@
 // plugin
 import Cookies from 'cookies-js';
-
-// 从 cookie 判断用户是否已经登录
-let token = Cookies.get('userToken');
-if (token) {
-  $('.notLogged').remove();
-  $('.hasLogged').removeClass('hide');
-} else {
-  $('.hasLogged').remove();
-  $('.notLogged').removeClass('hide');
-}
