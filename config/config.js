02e0425741b51951672e8623234d047beeb193f3
Biallo <bialloworld@gmail.com>
2017-05-10 14:21:55 +0800

    Update config

--- config/config.js
+++ config/config.js
@@ -10,11 +10,7 @@ var config = {
       name: 'gold-rock'
     },
     port: process.env.PORT || 3000,
-    weixin: 'http://app.reitschain.com',
-    // platform: 'http://192.168.1.178:8080/reits',
-    platform: 'http://123.207.164.141:9080/reits',
-    haobtcClientId: '2e10c0dfb0b54be1a312',
-    haobtcClientSecret: 'a760851fce234b1bae336cbe69c2a4faa200bbe5'
+    platform: 'http://10.0.2.80:8080'
   },
   beta: {
     root: rootPath,
@@ -23,10 +19,7 @@ var config = {
       name: 'gold-rock'
     },
     port: process.env.PORT || 3000,
-    weixin: 'http://app.reitschain.com',
-    platform: 'http://123.207.164.141:9080/reits',
-    haobtcClientId: '008c59a9491643f6b5e3',
-    haobtcClientSecret: '49e5757ff78441c894df8020e168dd817296885f'
+    platform: 'http://123.207.164.141:9080/reits'
   },
   production: {
     root: rootPath,
@@ -35,10 +28,7 @@ var config = {
       name: 'gold-rock'
     },
     port: process.env.PORT || 3000,
-    weixin: 'http://app.reitschain.com',
-    platform: 'https://box.reitschain.com/reits',
-    haobtcClientId: '908f8052f055442487f7',
-    haobtcClientSecret: '59362bbdac004d9d82c2823e211b07a5d85f0e6b'
+    platform: 'https://box.reitschain.com/reits'
   }
 };
 
